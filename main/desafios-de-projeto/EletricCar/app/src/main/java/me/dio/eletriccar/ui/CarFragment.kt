76c4dec06d6f356dec8fc38bdaac88748cf06a24
adevan-neves-santos <adevannevessantos15@gmail.com>
2022-12-25 19:38:22 -0400

    Finaliza conexão com Api e empty state na tela do App.

--- main/desafios-de-projeto/EletricCar/app/src/main/java/me/dio/eletriccar/ui/CarFragment.kt
+++ main/desafios-de-projeto/EletricCar/app/src/main/java/me/dio/eletriccar/ui/CarFragment.kt
@@ -1,12 +1,20 @@
 package me.dio.eletriccar.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -27,6 +35,9 @@ class CarFragment : Fragment() {
 
     lateinit var btnCalcular: FloatingActionButton
     lateinit var listaCarros: RecyclerView
+    lateinit var progress: ProgressBar
+    lateinit var noInternetImage: ImageView
+    lateinit var noInternetText: TextView
 
     var carrosArray:ArrayList<Carro> = ArrayList()
 
@@ -45,9 +56,24 @@ class CarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        callService()
         setupView()
         setupListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(checkForInternet(context)){
+            callService()
+        } else {
+            emptyState()
+        }
+    }
+
+    private fun emptyState(){
+        progress.isVisible = false
+        listaCarros.isVisible = false
+        noInternetImage.isVisible = true
+        noInternetText.isVisible = true
     }
 
     override fun onDestroyView() {
@@ -56,13 +82,19 @@ class CarFragment : Fragment() {
     }
 
     private fun setupView(){
-        btnCalcular  = binding.fabCalcular
-        listaCarros  = binding.rvListaCarros
+        btnCalcular     = binding.fabCalcular
+        listaCarros     = binding.rvListaCarros
+        progress        = binding.pbLoader
+        noInternetImage = binding.ivEmptyState
+        noInternetText  = binding.tvNoWifi
     }
 
     private fun setupList(){
-        val adapter = CarAdapter(carrosArray)
-        listaCarros.adapter = adapter
+        val carroAdapter = CarAdapter(carrosArray)
+        listaCarros.apply {
+            isVisible = true
+            adapter = carroAdapter
+        }
     }
 
     private fun setupListeners() {
@@ -76,11 +108,28 @@ class CarFragment : Fragment() {
         MyTask().execute(urlBase)
     }
 
+    private fun checkForInternet(context: Context?) : Boolean{
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            return networkInfo.isConnected
+        }
+    }
+
     inner class MyTask:AsyncTask<String,String,String>(){
 
         override fun onPreExecute() {
             super.onPreExecute()
             Log.d("MyTask","Iniciando ...")
+            progress.isVisible = true
         }
 
         override fun doInBackground(vararg url: String?): String {
@@ -90,9 +139,19 @@ class CarFragment : Fragment() {
                 urlConnection = urlBase.openConnection() as HttpURLConnection
                 urlConnection.connectTimeout = 60000
                 urlConnection.readTimeout = 60000
+                urlConnection.setRequestProperty(
+                     "Accept"
+                    ,"application/json"
+                )
 
-                var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
-                publishProgress(response)
+                val respondeCode = urlConnection.responseCode
+                if(respondeCode == HttpURLConnection.HTTP_OK){
+                    var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
+                    publishProgress(response)
+                } else {
+                    Log.e("Erro","Serviço indisponível no momento ...")
+                }
+
             } catch (ex:Exception){
                 Log.e("Erro","Erro ao realizar processamento ...")
             } finally {
@@ -103,6 +162,7 @@ class CarFragment : Fragment() {
 
         override fun onProgressUpdate(vararg values: String?) {
             try {
+                carrosArray = ArrayList()
                 val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
                 for(i in 0 until jsonArray.length()){
                     val id = jsonArray.getJSONObject(i).getString("id")
@@ -134,11 +194,13 @@ class CarFragment : Fragment() {
                     carrosArray.add(model)
                     Log.d("Model -> ",model.toString())
                 }
+                progress.isVisible        = false
+                noInternetImage.isVisible = false
+                noInternetText.isVisible  = false
                 setupList()
             } catch (ex:Exception){
                 Log.e("Erro",ex.message.toString())
             }
         }
     }
-
 }
